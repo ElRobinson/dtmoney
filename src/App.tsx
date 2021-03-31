74bc81ccd735a3f6ebd2bc0c74934aaa0db60fca
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2021-03-30 22:01:57 -0300

    Added styled components and global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import React from "react";
+import { GlobalStyle } from "./styles/global";
+
+
 export function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <GlobalStyle/>
     </div>
   );
 }
