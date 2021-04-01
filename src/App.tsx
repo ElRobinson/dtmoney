d1e4fa038ec987afd419c120029705504fdf546c
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2021-03-31 22:19:09 -0300

    Added Components and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+
 import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-      <GlobalStyle/>
-    </div>
+    <>
+      <Header />
+      <Dashboard/>
+      <GlobalStyle />
+    </>
   );
 }
 
---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Summary } from "../Summary"
+
+import { Container } from "./styles"
+
+export function Dashboard(){
+    return (
+        <Container>
+            <Summary/>
+        </Container>
+    )
+}
